8db9d752284a65defed3a05c11b114aca73190fa
Andrew Ma <andrewmamusic@gmail.com>
2024-10-24 15:44:53 -0400

    assignment 2 updates

--- homework/two/src/Browser.java
+++ homework/two/src/Browser.java
@@ -8,9 +8,9 @@ public class Browser {
     private Stack<UrlInfo> pageForwardStack = new ArrayStack<>();
     private UrlInfo currentPage;
 
-    // Stack for back/forward on page
-    private Stack<String> frameBackStack = new ArrayStack<>();
-    private Stack<String> frameForwardStack = new ArrayStack<>();
+    // Stack for prev/next on page
+    private Stack<String> framePrevStack = new ArrayStack<>();
+    private Stack<String> frameNextStack = new ArrayStack<>();
     private String currentFrame;
 
     private TextRenderer renderer;
@@ -42,34 +42,158 @@ public class Browser {
             System.out.println(output);
         }
     }
+    // function to go to the next frame
+    public String goNext() {
+
+        // if the stack is not empty, pop the next frame
+        // if the current frame is not null, push it to the prev stack
+        // set the current frame to the next frame
+        if (!this.frameNextStack.isEmpty()) {
+            if (this.currentFrame != null) {
+                this.framePrevStack.push(this.currentFrame);
+            }
+            this.currentFrame = this.frameNextStack.pop();
+        }
+        else {
+            // if the renderer has a next frame, set the current frame to the next frame
+            // if the current frame is not null, push it to the prev stack
+            if (this.renderer.hasNextFrame()) {
+                if (this.currentFrame != null) {
+                    this.framePrevStack.push(this.currentFrame);
+                }
+                this.currentFrame = this.renderer.nextFrame();
+            }
+
+            // otherwise, return that the end of the page has been reached
+            else {
+                return "End of page reached";
+            }
+        }
+
+        return this.currentFrame;
+    }
+
+    public String navPage(UrlInfo page) {
+        // if the renderer cannot load the page, return that it failed
+        if (!this.renderer.newPage(page)) {
+            return "Failed to load the page.";
+        }
+
+        // if the current page is not null, push it to the back stack
+        if (this.currentPage != null) {
+            this.pageBackStack.push(this.currentPage);
+        }
+
+        // set the current page to the new page
+        this.currentPage = page;
+
+        // clear the forward stack
+        this.pageForwardStack.clear();
+
+        // go to the next frame
+        return this.goNext();
+    }
 
     public String runCommand(String command) {
         switch (command.toLowerCase()) {
             case "":
             case "next":
-                return "NEXT not implemented";
+                // go to the next frame
+                return this.goNext();
             case "home":
-                return "HOME not implemented";
+                // go to the home page
+                return this.navPage(this.homepage);
             case "prev":
-                return "PREV not implemented";
+                // if the prev stack is not empty, pop the previous frame
+                // if the current frame is not null, push it to the next stack
+                // set the current frame to the previous frame
+                if (!this.framePrevStack.isEmpty()) {
+                    if (this.currentFrame != null) {
+                        this.frameNextStack.push(this.currentFrame);
+                    }
+                    this.currentFrame = this.framePrevStack.pop();
+                }
+                else {
+                    // if the top of the page has been reached, return that it has
+                    return "Top of page reached";
+                }
+
+                // return the current frame
+                return this.currentFrame;
             case "reload":
-                return "PREV not implemented";
-            case "back":
-                return "BACK not implemented";
+                // reload the current page
+                return this.navPage(this.currentPage);  
+            case "back":    
+                // if the back stack is not empty, pop the previous page
+                // if the current page is not null, push it to the forward stack
+                // set the current page to the previous page
+                if (!this.pageBackStack.isEmpty()) {
+                    if (this.currentPage != null) {
+                        this.pageForwardStack.push(this.currentPage);
+                    }
+                    this.currentPage = this.pageBackStack.pop();
+                }
+                else {
+                    // if there is no page to go back to, return that it failed
+                    return "No page to go back to.";
+                }
+
+                // return the current page
+                return this.navPage(this.currentPage);     
             case "forward":
-                return "FORWARD not implemented";
+                // if the forward stack is not empty, pop the next page
+                // if the current page is not null, push it to the back stack
+                // set the current page to the next page
+                if (!this.pageForwardStack.isEmpty()) {
+                    if (this.currentPage != null) {
+                        this.pageBackStack.push(this.currentPage);
+                    }
+                    this.currentPage = this.pageForwardStack.pop();
+                }
+                else {
+                    // if there is no page to go forward to, return that it failed
+                    return "No page to go forward to.";
+                }
+                
+                // return the current page
+                return this.navPage(this.currentPage);
             case "help":
+                // return the help menu
                 return showHelp();
             case "links":
-                return "LINKS not implemented";
+                // format the links
+                StringBuffer sb = new StringBuffer();
+
+                // for each link, format it and append it to the string buffer
+                for (int i = 0; i < this.renderer.links.size(); i++) {
+                    sb.append(this.formatLink(i, this.renderer.links.get(i)) + "\n");
+                }
+
+                // return the formatted links
+                return sb.toString();
+
             case "exit":
             case "quit":
+                // exit the program (both 'exit' and 'quit' are valid commands)
                 System.exit(0);
                 return "";
             default:
+                // if the command is a number, navigate to the link
                 if (isNumber(command)) {
-                    return "GO TO LINK " + command + " not implemented";
+                    // parse the command as an integer
+                    int com = Integer.parseInt(command);
+
+                    // if the command is not within the range of links, return that it is invalid
+                    if (!((com >= 0) && (com <= this.renderer.links.size()))) {
+                        return "Invalid link";
+                    }
+                    else {
+                        // navigate to the link
+                        UrlInfo newUrl = UrlInfo.navigate(this.currentPage.toString(), this.renderer.links.get(com));
+                        return this.navPage(newUrl);
+                    }
                 } else {
+                    // if the input is not a valid number or command, return that it is invalid
                     return "Invalid command. Type HELP for a list of commands.";    
                 }
         }
---
+++ homework/two/src/readme.txt
@@ -0,0 +1,25 @@
+The Information Superhighway
+October 24, 2024
+Data Structures (CSCI-UA 102)
+
+RUNBOOK: 
+This program can be compiled by executing the following command while in the two/src directory:
+javac *.java
+
+This program can be run by executing the following command while in the two/src directory:
+java Browser [number of frames]
+
+Example: java Browser 10
+
+TIME SPENT: 
+This project took around 3 hours to complete.
+
+NOTES:
+There was some difficulty initially with understanding how to properly implement the 
+different ArrayStacks to keep track of the pages and frames.
+
+RESOURCES AND ACKNOWLEDGEMENTS: 
+Similar to the previous project, the code in this project was submitted through ChatGPT to identify any exceptions
+or bugs that would have caused problems, particularly looking at any minor syntax
+or formatting errors. Additionally, I consulted my notes from CSCI-UA 101 to 
+review some basic syntax.
